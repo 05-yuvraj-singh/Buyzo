1c0eb344c2419224d32919720bab8b07d0041cdf
05-yuvraj-singh <yuvrajsingh532004@gmail.com>
2023-10-23 11:40:00 +0530

    products added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
+import data from "./data";
+import './index.css'
 function App() {
   return (
     <div >
       <header>
-        <a href='/'>Ecom</a>
+        <a href='/'>BuyZo</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+
+        {
+          data.products.map(products =>(
+            <div className="product" key={products.slug}>
+            <a href={`/product/${products.slug}`}>
+
+            <img src={products.image} alt ={products.name}></img>
+            </a>
+            <div className="product-info">
+            <a href={`/product/${products.slug}`}>
+            <p>{products.name}</p>            
+            </a>
+
+            <p><strong>
+            ${products.price}
+
+            </strong>
+            </p>
+            <button>Add to Cart</button>
+            </div>
+            </div>))
+        }
+        </div>
+        
       </main>
     </div>
   );
